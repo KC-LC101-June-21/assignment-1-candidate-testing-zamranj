d3adc8508d12cd30af07d9427d35a3bbd58e2e0f
zeejay21 <84418586+zamranj@users.noreply.github.com>
2021-06-10 21:51:54 +0000

    Completed Part 2 of the Candidate testing exercise

--- candidate-testing.js
+++ candidate-testing.js
@@ -5,12 +5,13 @@ const input = require('readline-sync');
 // TODO 1.1a: Define candidateName // 
 let candidateName = "";
 // TODO 1.2a: Define question, correctAnswer, and candidateAnswer //
-let question = "Who was the first American woman in space? ";
+let question = "";
 let correctAnswer = "Sally Ride";
 let candidateAnswer = "";
-let questions;
-let correctAnswers;
-let candidateAnswers;
+let questions = ["Who was the first American woman in space?","True or false: 5 kilometer == 5000 meters?","(5 + 3)/2 * 10 = ?","Given the array [8, 'Orbit', 'Trajectory', 45], what entry is at index 2?","What is the minimum crew size for the ISS?"];
+let correctAnswers =["Sally Ride", "true", "40","trajectory", "3"] ;
+let candidateAnswers=[];
+
 
 
 function askForName() {
@@ -20,22 +21,26 @@ function askForName() {
 
 function askQuestion() {
   // TODO 1.2b: Ask candidate the question and assign the response as candidateAnswer //
-candidateAnswer = input.question(question);
+for(let i = 0; i < questions.length; i++){
+ 
+  candidateAnswers[i] = input.question(`${i+1}) ${questions[i]}`);
+  console.log("your answer: ",candidateAnswers[i] );
+  
+ }
 
 }
 
 function gradeQuiz(candidateAnswers) {
 
   // TODO 1.2c: Let the candidate know if they have answered the question correctly or incorrectly // 
-  if(candidateAnswer === correctAnswer){
-    console.log("Correct!");
+if(candidateAnswers){
+    console.log(`Correct!`);
   }else{
-    console.log("Correct Answer: "+ correctAnswer);
+    console.log(`Correct Answer: ${correctAnswers}`);
   }
-
-  let grade;
+let grade;
   
-
+ 
   return grade;
 }
 
@@ -44,7 +49,6 @@ function runProgram() {
   // TODO 1.1c: Ask for candidate's name //
   console.log("Greetings, "+ candidateName)
   askQuestion();
-  gradeQuiz(this.candidateAnswers);
 }
 
 // Don't write any code below this line //
