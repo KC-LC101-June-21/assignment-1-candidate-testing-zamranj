8852edbd145e486bc5750ac177dd4728ab212900
zeejay21 <84418586+zamranj@users.noreply.github.com>
2021-06-11 03:42:04 +0000

    fix for part 1.2c:

--- candidate-testing.js
+++ candidate-testing.js
@@ -9,7 +9,7 @@ let question = "";
 let correctAnswer = "Sally Ride";
 let candidateAnswer = "";
 let questions = ["Who was the first American woman in space?","True or false: 5 kilometer == 5000 meters?","(5 + 3)/2 * 10 = ?","Given the array [8, 'Orbit', 'Trajectory', 45], what entry is at index 2?","What is the minimum crew size for the ISS?"];
-let correctAnswers =["Sally Ride", "true", "40","trajectory", "3"] ;
+let correctAnswers =["Sally Ride", "true", "40", "Trajectory", "3"] ;
 let candidateAnswers=[];
 
 
@@ -33,12 +33,10 @@ for(let i = 0; i < questions.length; i++){
 function gradeQuiz(candidateAnswers) {
 
   // TODO 1.2c: Let the candidate know if they have answered the question correctly or incorrectly // 
-for(let i = 0; i < candidateAnswers.length; i++){
-  if(candidateAnswers[i]===correctAnswers[i]){
-    console.log(`Correct!`);
-  }else{
-    console.log(`Correct Answer: ${correctAnswers[i]}`);
-  }
+for(let i = 0; i <candidateAnswers.length; i++){
+  console.log(questions[i]);
+  console.log(`Candidates answer ${candidateAnswers[i]}`);
+  console.log(`Correct Answer: ${correctAnswers[i]}\n`);
 }
 
 let grade;
@@ -52,7 +50,7 @@ function runProgram() {
   // TODO 1.1c: Ask for candidate's name //
   console.log("Greetings, "+ candidateName)
   askQuestion();
-  
+  gradeQuiz(candidateAnswers);
 }
 
 // Don't write any code below this line //
